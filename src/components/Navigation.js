ba6967ed6ca866d90c0a69f95c9c003d37f088bf
soeun <bentluxmea@gmail.com>
2021-06-19 05:44:01 +0900

    충돌수정

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,27 +5,25 @@ import logo from "../assets/logo.gif";
 
 const Navigation = ({userObj, isLoggedIn}) => {
   return (<nav className="naviContainer">
-    {/* Left Side Nav */}
-    {
-      isLoggedIn && <div className="naviSideContainer">
-        <Link className="navLink" to='/profile'>
-          Profile
-        </Link>
-      </div>
-    }
-    {/* Center Logo */}
+    <div className="naviLeftContainer">
+        {
+          isLoggedIn && 
+            <Link className="navLink" to='/profile'>
+              Profile
+            </Link>
+        }
+        {
+          isLoggedIn && 
+            <Link className="navLink" to="/ranking">
+              Ranking
+            </Link>
+        }
+    </div>
     <div>
       <Link className="navLink" to="/">
         <img className="logo" src={logo}/>
       </Link>
     </div>
-    {
-      isLoggedIn && <div className="naviSideContainer">
-        <Link className="navLink" to="/ranking">
-          Ranking
-        </Link>
-      </div>
-    }
   </nav>);
 };
 
