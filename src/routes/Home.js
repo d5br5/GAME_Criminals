5e5ec6e54f5b03cf245122b386982d5f5743a8f7
DohyungKim <ehgud456456@naver.com>
2021-06-19 23:04:53 +0900

    homepage game선택 화면

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,17 +7,30 @@ const Home = ({isLoggedIn}) => {
   return (
     <div className="body">
       {
-        isLoggedIn ? <>
+        isLoggedIn ? <div className='mainPage'>
           <Link to='/game1'>
-            <button className="btnPlayGame">GAME1</button>
+            <div className="mainGameIntro">
+              <h1>Game 1</h1>
+              <h2> 범죄자 죄목 추측</h2>
+              <p>2가지 선택지 중 실제 죄목은?</p>
+              <p>10라운드로 구성</p>
+            </div>
           </Link>
+          <span style={{width:"100px"}}></span>
           <Link to='/game2'>
-            <button className="btnPlayGame">GAME2</button>
+            <div className="mainGameIntro">
+              <h1>Game 2</h1>
+              <h2> 범죄자 형량 대결</h2>
+              <p>두 사람 중 더 형량이 센 사람은?</p>
+              <p>5라운드로 구성</p>
+            </div>
           </Link>
-        </> : <Auth/>
+
+        </div> : <Auth/>
       }
     </div>
   );
-};
+}
+;
 
 export default Home;
